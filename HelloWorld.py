2b0e5ae89e346818027592a054ee1ffdfce37fd5
BRMehta <mehtabhargavr@gmail.com>
2022-02-15 19:17:50 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,11 +2,6 @@
 # This Phython program will print Hellow World...
 print("Hello World Demo...15 Febuary 2022 \n")
 print("Hello World Demo...15 Febuary 2022 \n")
-print("Hello World Demo...15 Febuary 2022 \n")
-print("Hello World Demo...15 Febuary 2022 \n")
-print("Hello World Demo...15 Febuary 2022 \n")
-print("Hello World Demo...15 Febuary 2022 \n")
-
 
 
 
